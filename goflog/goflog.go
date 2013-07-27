af2c3b99cc77ddd67ab696fb468ce6fca62f029e
Vika Zhou <vika.zhou@cargosmart.com>
2013-07-27 18:13:18 +0800

    move proxy code to webproxy.og

--- goflog/goflog.go
+++ goflog/goflog.go
@@ -1,77 +1,75 @@
 package goflog
 
 import (
-    "appengine"
-    "appengine/datastore"
-    "appengine/urlfetch"
-    "appengine/user"
-    "fmt"
-    "html/template"
-    "io"
-    "io/ioutil"
-    "log"
-    "net"
-    "net/http"
-    "net/url"
-    "strconv"
-    //"path/filepath"
-    "time"
-    _ "errors"
-    //"os"
-    "strings"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	//"path/filepath"
+	_ "errors"
+	"time"
+	//"os"
+	"strings"
 )
 
 type Greeting struct {
-    Author  string
-    Content string
-    Date    time.Time
+	Author  string
+	Content string
+	Date    time.Time
 }
 
 const (
-    TEMPLATE_DIR string = "templates"
-    TEMPLATE_THEME_DIR string = "templates/themes/twentyten"
-    TEMPLATE_EXT string = ".html"
+	TEMPLATE_DIR       string = "templates"
+	TEMPLATE_THEME_DIR string = "templates/themes/twentyten"
+	TEMPLATE_EXT       string = ".html"
 )
-var (
-    /* templates = template.Must(template.ParseFiles(
-        "templates/home.html",
-        "templates/admin.html",
-        "templates/post_edit.html",
-    ))*/
-    templates = template.Must(template.ParseFiles(
-        "templates/themes/twentyten/index.html",
-        "templates/themes/twentyten/header.html",
-        "templates/themes/twentyten/footer.html",
-        "templates/themes/twentyten/loop.html",
-        "templates/themes/twentyten/sidebar.html",
-    ))
-    //tmplPostList = template.Must(template.ParseFiles("templates/post_list.html"))
 
-    tmplPost     = template.Must(template.ParseFiles(
-        "templates/themes/twentyten/single.html",
-        "templates/themes/twentyten/header.html",
-        "templates/themes/twentyten/footer.html",
-        "templates/themes/twentyten/loop-single.html",
-        "templates/themes/twentyten/sidebar.html",
-        "templates/themes/twentyten/disqus-comment.html",
-        //"templates/themes/twentyten/comments.html",
-        //"templates/themes/twentyten/comment-form.html",
-    ))
-    tmpl404 = template.Must(template.ParseFiles(
-        "templates/themes/twentyten/404.html",
-        "templates/themes/twentyten/header.html",
-        "templates/themes/twentyten/footer.html",
-    ))
-    //tmplTerm = template.Must(template.ParseFiles("templates/term.html"))
-    blog     = make(map[string]string)
+var (
+	/* templates = template.Must(template.ParseFiles(
+	    "templates/home.html",
+	    "templates/admin.html",
+	    "templates/post_edit.html",
+	))*/
+	templates = template.Must(template.ParseFiles(
+		"templates/themes/twentyten/index.html",
+		"templates/themes/twentyten/header.html",
+		"templates/themes/twentyten/footer.html",
+		"templates/themes/twentyten/loop.html",
+		"templates/themes/twentyten/sidebar.html",
+	))
+	//tmplPostList = template.Must(template.ParseFiles("templates/post_list.html"))
+
+	tmplPost = template.Must(template.ParseFiles(
+		"templates/themes/twentyten/single.html",
+		"templates/themes/twentyten/header.html",
+		"templates/themes/twentyten/footer.html",
+		"templates/themes/twentyten/loop-single.html",
+		"templates/themes/twentyten/sidebar.html",
+		"templates/themes/twentyten/disqus-comment.html",
+		//"templates/themes/twentyten/comments.html",
+		//"templates/themes/twentyten/comment-form.html",
+	))
+	tmpl404 = template.Must(template.ParseFiles(
+		"templates/themes/twentyten/404.html",
+		"templates/themes/twentyten/header.html",
+		"templates/themes/twentyten/footer.html",
+	))
+	//tmplTerm = template.Must(template.ParseFiles("templates/term.html"))
+	blog = make(map[string]string)
 )
 
 //func initTemplate() {
-    /*fi, err := os.Stat(TEMPLATE_DIR)
-    if err != nil || !fi.IsDir(){
-        log.Printf("Directory %v not exists: %v", TEMPLATE_DIR, err)
-        return
-    }*/
+/*fi, err := os.Stat(TEMPLATE_DIR)
+  if err != nil || !fi.IsDir(){
+      log.Printf("Directory %v not exists: %v", TEMPLATE_DIR, err)
+      return
+  }*/
 /*    fis, err := ioutil.ReadDir(TEMPLATE_DIR)
     if err != nil {
         log.Printf("Directory %v not exists: %v", TEMPLATE_DIR, err)
@@ -88,239 +86,184 @@ var (
 }*/
 
 func init() {
-    blog["charset"] = "UTF-8"
-    blog["name"] = "Vika's Blog"
-    blog["description"] = "a longer way"
-    blog["siteurl"] = ""
+	blog["charset"] = "UTF-8"
+	blog["name"] = "Vika's Blog"
+	blog["description"] = "a longer way"
+	blog["siteurl"] = ""
 
-    //initTemplate()
+	//initTemplate()
 
-    http.HandleFunc("/", handleHome)
-    http.HandleFunc("/post", handleViewPost)
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/post", handleViewPost)
 
-    http.HandleFunc("/admin/", admin)
-    http.HandleFunc("/admin/post", handlePostList)
-    http.HandleFunc("/admin/post/edit", postEdit)
-    http.HandleFunc("/admin/term", handleTerm)
-    http.HandleFunc("/admin/term/edit", handleTermEdit)
-    http.HandleFunc("/admin/export", handleExport)
-    http.HandleFunc("/admin/maintain", handleMaintain)
+	http.HandleFunc("/admin/", admin)
+	http.HandleFunc("/admin/post", handlePostList)
+	http.HandleFunc("/admin/post/edit", postEdit)
+	http.HandleFunc("/admin/term", handleTerm)
+	http.HandleFunc("/admin/term/edit", handleTermEdit)
+	http.HandleFunc("/admin/export", handleExport)
+	http.HandleFunc("/admin/maintain", handleMaintain)
 
-
-    http.HandleFunc("/blog/", handleProxy)
-
-    //http.HandleFunc("/welcome", welcome)
-    //http.HandleFunc("/guest", guestHandler)
-    //http.HandleFunc("/sign", sign)
-    //http.HandleFunc("/_ah/login_required", openIdHandler)
+	//http.HandleFunc("/welcome", welcome)
+	//http.HandleFunc("/guest", guestHandler)
+	//http.HandleFunc("/sign", sign)
+	//http.HandleFunc("/_ah/login_required", openIdHandler)
 }
 
 func serveError(c appengine.Context, w http.ResponseWriter, err error) {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    io.WriteString(w, "Internal Server Error")
-    c.Errorf("%v", err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(w, "Internal Server Error")
+	c.Errorf("%v", err)
 }
 
 func serveNotFound(w http.ResponseWriter, r *http.Request) {
-    data := make(map[string]interface{})
-    data["Blog"] = blog
-    w.WriteHeader(http.StatusNotFound)
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    tmpl404.Execute(w, data)
+	data := make(map[string]interface{})
+	data["Blog"] = blog
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmpl404.Execute(w, data)
 }
-
-// proxy of viiflog
-func handleProxy(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  client := urlfetch.Client(c)
-
-  target_url := "http://localhost:8080"
-  if true {
-    target_url = "http://viiflog.appspot.com"
-  }
-  targetURL,err := url.Parse(target_url)
-
-  outreq := new(http.Request)
-  *outreq = *r
-  outreq.URL.Scheme = targetURL.Scheme
-  outreq.URL.Host = targetURL.Host
-  //outreq.URL.Path = r.URL.Path
-  //outreq.URL.RawQuery = r.URL.RawQuery
-  
-  // Request.RequestURI can't be set in client requests.
-  outreq.RequestURI = ""
-
-  if clientIp, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-     outreq.Header.Set("X-Forwarded-For", clientIp)
-  }
-  outreq.Header.Set("X-Viifly", "http://blog.viifly.com")
-
-  //resp, err := http.DefaultClient.Do(outreq)
-  resp, err := client.Do(outreq)
-  if err != nil {
-    //panic(err)
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  if resp.Header != nil {
-    for k, v := range resp.Header {
-      for _, vv := range v {
-        w.Header().Add(k, vv)
-      }
-    }
-  }
-
-  w.WriteHeader(resp.StatusCode)
-  defer resp.Body.Close()
-  result, err := ioutil.ReadAll(resp.Body)
-  if err != nil && err != io.EOF {
-    panic(err)
-  }
-  w.Write(result)
-}
-
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    //handle for path all
-    if r.URL.Path != "/" {
-        if ref := r.Header.Get("Referer"); ref != "" {
-            c.Debugf("Referer: %v", ref)
-            if refURL,err := url.Parse(ref); err == nil {
-                if strings.Index(refURL.Path, "/webproxy/") >= 0 {
-                    q := refURL.Query()
-                    prxyForUrlString := q.Get("url")
-                    prxyForURL, err :=  url.Parse(prxyForUrlString)
-                    if err == nil {
-                        prxyForURL.Path =  r.URL.Path
-                        prxyForURL.RawQuery =  r.URL.RawQuery
-                        prxyForURL.Fragment = r.URL.Fragment
-                        q.Set("url", prxyForURL.String())
-                        refURL.RawQuery = q.Encode()
-                        r.URL = refURL
-                        r.Header.Set("Referer", prxyForUrlString)
-                        //http.Redirect(w, r, refURL.String(), http.StatusFound)
-                        handleWebProxy(w, r)
-                        return
-                    }
-                }        
-            }
-            
-        }
-        
-        serveNotFound(w, r)
-        return
-    }
+	//handle for path all
+	if r.URL.Path != "/" {
+		if ref := r.Header.Get("Referer"); ref != "" {
+			c.Debugf("Referer: %v", ref)
+			if refURL, err := url.Parse(ref); err == nil {
+				if strings.Index(refURL.Path, "/webproxy/") >= 0 {
+					q := refURL.Query()
+					prxyForUrlString := q.Get("url")
+					prxyForURL, err := url.Parse(prxyForUrlString)
+					if err == nil {
+						prxyForURL.Path = r.URL.Path
+						prxyForURL.RawQuery = r.URL.RawQuery
+						prxyForURL.Fragment = r.URL.Fragment
+						q.Set("url", prxyForURL.String())
+						refURL.RawQuery = q.Encode()
+						r.URL = refURL
+						r.Header.Set("Referer", prxyForUrlString)
+						//http.Redirect(w, r, refURL.String(), http.StatusFound)
+						handleWebProxy(w, r)
+						return
+					}
+				}
+			}
 
-    posts, err := GetLatestPosts(c, 10, true)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+		}
 
-    /*if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }*/
-    /* if err := templates.ExecuteTemplate(w, "home.html", posts); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }*/
+		serveNotFound(w, r)
+		return
+	}
 
-    /* for i := 0; i < len(posts); i++ {
-       posts[i].HTMLContent = template.HTML(posts[i].Content)
-     }*/
+	posts, err := GetLatestPosts(c, 10, true)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    data := make(map[string]interface{})
-    data["Posts"] = posts
-    data["Blog"] = blog
-    data["Categories"] = GetCategories(c)
-    if err := templates.Execute(w, data); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	/*if err := guestbookTemplate.Execute(w, greetings); err != nil {
+	    http.Error(w, err.Error(), http.StatusInternalServerError)
+	}*/
+	/* if err := templates.ExecuteTemplate(w, "home.html", posts); err != nil {
+	    http.Error(w, err.Error(), http.StatusInternalServerError)
+	}*/
+
+	/* for i := 0; i < len(posts); i++ {
+	   posts[i].HTMLContent = template.HTML(posts[i].Content)
+	 }*/
+
+	data := make(map[string]interface{})
+	data["Posts"] = posts
+	data["Blog"] = blog
+	data["Categories"] = GetCategories(c)
+	if err := templates.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func handleViewPost(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    var postID int64
-    var post *Post = nil
-    //var postKey *datastore.Key
+	c := appengine.NewContext(r)
+	var postID int64
+	var post *Post = nil
+	//var postKey *datastore.Key
 
-    idStr := r.FormValue("id")
-    cateIDStr := r.FormValue("category")
-    singlePost := true
-    var posts []Post
+	idStr := r.FormValue("id")
+	cateIDStr := r.FormValue("category")
+	singlePost := true
+	var posts []Post
 
-    if i, err := strconv.Atoi(idStr); err != nil {
-        c.Infof("Failed to convert str to int64: ", i)
-    } else {
-        postID = int64(i)
-        //postKey = CreatePostKey(c, postID)
-        post = GetPostByID(c, postID)
-    }
-    if post != nil && post.Published {
-        //posts[0] = *post
-        posts = append(posts, *post)
-    } else if cateIDStr != "" {
-        if cateID, err := strconv.Atoi(cateIDStr); err == nil {
-            posts = GetPostByCategory(c, int64(cateID), true)
-        }
+	if i, err := strconv.Atoi(idStr); err != nil {
+		c.Infof("Failed to convert str to int64: ", i)
+	} else {
+		postID = int64(i)
+		//postKey = CreatePostKey(c, postID)
+		post = GetPostByID(c, postID)
+	}
+	if post != nil && post.Published {
+		//posts[0] = *post
+		posts = append(posts, *post)
+	} else if cateIDStr != "" {
+		if cateID, err := strconv.Atoi(cateIDStr); err == nil {
+			posts = GetPostByCategory(c, int64(cateID), true)
+		}
 
-        singlePost = false
-    } else {
-        log.Print("Post not found for URL", r.URL.Path)
-        serveNotFound(w, r)
-        return
-    }
+		singlePost = false
+	} else {
+		log.Print("Post not found for URL", r.URL.Path)
+		serveNotFound(w, r)
+		return
+	}
 
-    model := struct {
-        Posts      []Post
-        Blog       map[string]string
-        Categories []Term
-    }{
-        posts, blog, GetCategories(c),
-    }
+	model := struct {
+		Posts      []Post
+		Blog       map[string]string
+		Categories []Term
+	}{
+		posts, blog, GetCategories(c),
+	}
 
-    tmpl := tmplPost
-    if !singlePost {
-        tmpl = templates
-    }
+	tmpl := tmplPost
+	if !singlePost {
+		tmpl = templates
+	}
 
-    if err := tmpl.Execute(w, model); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	if err := tmpl.Execute(w, model); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
-    if _, err := q.GetAll(c, &greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    /*if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }*/
-    if err := templates.ExecuteTemplate(w, "home.html", greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	/*if err := guestbookTemplate.Execute(w, greetings); err != nil {
+	    http.Error(w, err.Error(), http.StatusInternalServerError)
+	}*/
+	if err := templates.ExecuteTemplate(w, "home.html", greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
-    if _, err := q.GetAll(c, &greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
-    //fmt.Fprint(w, gustbookForm)
+	//fmt.Fprint(w, gustbookForm)
 }
 
 var guestbookTemplate = template.Must(template.New("sign").Parse(guestbookTemplateHTML))
@@ -356,27 +299,27 @@ const gustbookForm = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    g := Greeting{
-        Content: r.FormValue("content"),
-        Date:    time.Now(),
-    }
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
 
-    if u := user.Current(c); u != nil {
-        g.Author = u.String()
-    }
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
 
-    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    /*err := signTemplate.Execute(w, r.FormValue("content"))
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-      }*/
-    http.Redirect(w, r, "/", http.StatusFound)
+	/*err := signTemplate.Execute(w, r.FormValue("content"))
+	  if err != nil {
+	      http.Error(w, err.Error(), http.StatusInternalServerError)
+	  }*/
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
@@ -391,17 +334,17 @@ const signTemplateHTML = `
 `
 
 func guestHandler(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
-    }
-    fmt.Fprint(w, "Hello, world!", u)
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprint(w, "Hello, world!", u)
 }
--- goflog/webproxy.go
+++ goflog/webproxy.go
@@ -1,13 +1,15 @@
 package goflog
 
 import (
-    "appengine"
-    "appengine/urlfetch"
-    "io"
-    "io/ioutil"
-    "net/http"
-    "fmt"
-    "strings"
+	"appengine"
+	"appengine/urlfetch"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 var prxyPage = `
@@ -30,57 +32,99 @@ window.location.href="/admin/webproxy/?url="+ encodeURIComponent(l);
 </html>
 `
 
-func handleWebProxy(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+func writeProxyResponse(c appengine.Context, w http.ResponseWriter, resp *http.Response) {
+	if resp.Header != nil {
+		for k, v := range resp.Header {
+			for _, vv := range v {
+				w.Header().Add(k, vv)
+			}
+		}
+	}
 
-    weburl := r.FormValue("url")
-    if weburl == "" {
-        w.Header().Set("Content-Type", "text/html;charset=utf-8")
-        //io.WriteString(w, prxyPage)
-        fmt.Fprint(w, prxyPage)
-        return
-    }
-    c.Debugf("Param url is: %v", weburl)
-
-    if !strings.HasPrefix(weburl, "http:") && !strings.HasPrefix(weburl, "https:") {
-        weburl = "http://" + weburl
-        var newURL = r.URL
-        q := newURL.Query()
-        q.Set("url", weburl)
-        newURL.RawQuery = q.Encode()
-
-        http.Redirect(w, r, newURL.String(), http.StatusFound)
-        return
-    }
-    c.Debugf("Proxy for: %v", weburl)
-
-    client := urlfetch.Client(c)
-    resp, err := client.Get(weburl)
-    if err != nil {
-        c.Errorf("Error in handleWebProxy when client.Get(weburl), %v", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    //fmt.Fprintf(w, "http GET returned status %v", resp.Status)
-    if resp.Header != nil {
-    for k, v := range resp.Header {
-      for _, vv := range v {
-        w.Header().Add(k, vv)
-      }
-    }
-  }
-
-  w.WriteHeader(resp.StatusCode)
-  defer resp.Body.Close()
-  result, err := ioutil.ReadAll(resp.Body)
-  if err != nil && err != io.EOF {
-    c.Errorf("Error in handleWebProxy, %v", err)
-    panic(err)
-  }
-  w.Write(result)
+	w.WriteHeader(resp.StatusCode)
+	defer resp.Body.Close()
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil && err != io.EOF {
+		c.Errorf("Error when write proxy response %v", err)
+		panic(err)
+	}
+	w.Write(result)
 }
 
+func handleWebProxy(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 
-func init(){
-    http.HandleFunc("/admin/webproxy/", handleWebProxy)
+	weburl := r.FormValue("url")
+	if weburl == "" {
+		w.Header().Set("Content-Type", "text/html;charset=utf-8")
+		//io.WriteString(w, prxyPage)
+		fmt.Fprint(w, prxyPage)
+		return
+	}
+	c.Debugf("Param url is: %v", weburl)
+
+	if !strings.HasPrefix(weburl, "http:") && !strings.HasPrefix(weburl, "https:") {
+		weburl = "http://" + weburl
+		var newURL = r.URL
+		q := newURL.Query()
+		q.Set("url", weburl)
+		newURL.RawQuery = q.Encode()
+
+		http.Redirect(w, r, newURL.String(), http.StatusFound)
+		return
+	}
+	c.Debugf("Proxy for: %v", weburl)
+
+	client := urlfetch.Client(c)
+	resp, err := client.Get(weburl)
+	if err != nil {
+		c.Errorf("Error in handleWebProxy when client.Get(weburl), %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	c.Debugf("http GET returned status %v", resp.Status)
+	writeProxyResponse(c, w, resp)
+}
+
+// proxy of viiflog
+func handleBlogProxy(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+
+	target_url := "http://localhost:8080"
+	if true {
+		target_url = "http://viiflog.appspot.com"
+	}
+	targetURL, err := url.Parse(target_url)
+
+	outreq := new(http.Request)
+	*outreq = *r
+	outreq.URL.Scheme = targetURL.Scheme
+	outreq.URL.Host = targetURL.Host
+	//outreq.URL.Path = r.URL.Path
+	//outreq.URL.RawQuery = r.URL.RawQuery
+
+	// Request.RequestURI can't be set in client requests.
+	outreq.RequestURI = ""
+
+	if clientIp, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		outreq.Header.Set("X-Forwarded-For", clientIp)
+	}
+	outreq.Header.Set("X-Viifly", "http://blog.viifly.com")
+
+	//resp, err := http.DefaultClient.Do(outreq)
+	resp, err := client.Do(outreq)
+	if err != nil {
+		//panic(err)
+		c.Errorf("Error in when client.Do(outreq), %v", err)
+		http.Error(w, "Sorry, Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	writeProxyResponse(c, w, resp)
+}
+
+func init() {
+	http.HandleFunc("/blog/", handleBlogProxy)
+	http.HandleFunc("/admin/webproxy/", handleWebProxy)
 }
